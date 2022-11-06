7eed0446139eb05b92588c37daec83778a4aa0ff
bhavsarvishwa <vishwa.bhavsar29@gmail.com>
2022-11-06 12:58:02 -0500

    Update Dive.py

--- Dive.py
+++ Dive.py
@@ -1,4 +1,4 @@
-def dive_pt1():
+def dive_pt():
     with open("inputpt1.txt", 'r') as f:
         depth = 0
         horizontal = 0
@@ -13,23 +13,7 @@ def dive_pt1():
         return abs(depth * horizontal)
 
 
-def dive_pt2():
-    with open("inputpt1.txt", 'r') as f:
-        depth = 0
-        horizontal = 0
-        aim = 0
-        lines = f.readlines()
-        for line in lines:
-            if "forward" in line:
-                forward_value = int(line.split(" ")[1])
-                horizontal += forward_value
-                depth += aim * forward_value
-            if "down" in line:
-                aim += int(line.split(" ")[1])
-            if "up" in line:
-                aim -= int(line.split(" ")[1])
-        return abs(depth * horizontal)
 
 
-print(f'Part one: {dive_pt1()}')
-print(f'Part one: {dive_pt2()}')
+print(f'Part one: {dive_pt()}')
+
